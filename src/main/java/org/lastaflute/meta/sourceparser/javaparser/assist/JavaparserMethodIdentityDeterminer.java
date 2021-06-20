db10ac85db0ab4a0939e479043fa8f6ebe482471
jflute <jflute@localhost>
2021-06-21 01:02:36 +0900

    resolve generic and CDef prefix headache of method identity

--- src/main/java/org/lastaflute/meta/sourceparser/javaparser/assist/JavaparserMethodIdentityDeterminer.java
+++ src/main/java/org/lastaflute/meta/sourceparser/javaparser/assist/JavaparserMethodIdentityDeterminer.java
@@ -19,6 +19,8 @@ import java.lang.reflect.Method;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.dbflute.util.Srl;
+
 import com.github.javaparser.ast.body.MethodDeclaration;
 
 /**
@@ -28,20 +30,59 @@ import com.github.javaparser.ast.body.MethodDeclaration;
  */
 public class JavaparserMethodIdentityDeterminer {
 
+    // ===================================================================================
+    //                                                                             Matches
+    //                                                                             =======
     public boolean matchesMethod(Method method, MethodDeclaration dec) {
         return buildMethodIdentityNative(method).equals(buildMethodIdentitySource(dec));
     }
 
+    // ===================================================================================
+    //                                                                            Identity
+    //                                                                            ========
     public String buildMethodIdentityNative(Method method) { // e.g. get$index(ProductsSearchForm)
-        final String methodName = method.getName();
-        final String paramExp = Stream.of(method.getParameters()).map(pr -> pr.getType().getSimpleName()).collect(Collectors.joining(","));
+        final String methodName = method.getName(); // e.g. get$index
+        final String paramExp = Stream.of(method.getParameters())
+                .map(pr -> pr.getType().getSimpleName()) // e.g. ProductsSearchForm, non generic
+                .collect(Collectors.joining(getParameterDelimiter()));
         return doBuildMethodIdentity(methodName, paramExp);
     }
 
     public String buildMethodIdentitySource(MethodDeclaration dec) { // e.g. get$index(ProductsSearchForm)
-        final String methodName = dec.getNameAsString();
-        final String paramExp = dec.getParameters().stream().map(pr -> pr.getType().toString()).collect(Collectors.joining(","));
+        final String methodName = dec.getNameAsString(); // e.g. get$index
+        final String paramExp = dec.getParameters().stream().map(pr -> {
+            final String plainExp = pr.getTypeAsString(); // e.g. ProductsSearchForm, may have generic type
+            return filterSourceParamExpIfNeeds(plainExp); // adjust source definition to compare with reflection expression
+        }).collect(Collectors.joining(getParameterDelimiter()));
         return doBuildMethodIdentity(methodName, paramExp);
+    }
+
+    // -----------------------------------------------------
+    //                                         Filter Source
+    //                                         -------------
+    protected String filterSourceParamExpIfNeeds(String oneParamExp) { // may have generic type
+        return removeCDefPrefixIfNeeds(removeGenericDefinitionIfNeeds(oneParamExp));
+    }
+
+    protected String removeGenericDefinitionIfNeeds(String oneParamExp) { // may have generic type
+        // generic uneeded for method identity as Java specification
+        return Srl.substringFirstFront(oneParamExp, "<"); // remove it if exists
+    }
+
+    protected String removeCDefPrefixIfNeeds(String oneParamExp) {
+        // #for_now jflute is there more smart way? (2021/06/21)
+        if (oneParamExp.contains("CDef.")) { // e.g. CDef.MemberStatus
+            return Srl.substringFirstRear(oneParamExp, "CDef."); // e.g. MemberStatus
+        } else {
+            return oneParamExp;
+        }
+    }
+
+    // -----------------------------------------------------
+    //                                          Assist Logic
+    //                                          ------------
+    protected String getParameterDelimiter() {
+        return ", ";
     }
 
     protected String doBuildMethodIdentity(String methodName, String paramExp) {
